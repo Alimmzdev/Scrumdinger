d064790122d730f2a7f189a8594f0068e94c7476
Alimmz <Alimmz.Bulut@outlook.com>
2025-10-26 10:00:56 +0330

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shahrooz Ansari on 10/26/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
