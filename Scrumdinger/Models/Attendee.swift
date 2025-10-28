667c1ef86834b3aa9dbd8aba5c97f38cc0858dfa
Alimmzdev <ali24mohammadzadeh@gmail.com>
2025-10-28 22:50:08 +0330

    feat: Add swift data for storing data on device for loading old data in every launch

---
+++ Scrumdinger/Models/Attendee.swift
@@ -0,0 +1,8 @@
+//
+//  Attendee.swift
+//  Scrumdinger
+//
+//  Created by macbook on 8/6/1404 AP.
+//
+
+import Foundation
---
+++ Scrumdinger/Preview Content/PreviewContainer.swift
@@ -0,0 +1,18 @@
+//
+//  PreviewContainer.swift
+//  Scrumdinger
+//
+//  Created by macbook on 8/6/1404 AP.
+//
+
+import SwiftUI
+
+struct PreviewContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreviewContainer()
+}
